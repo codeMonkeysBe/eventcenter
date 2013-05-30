c5b3663a894f8efef09c3d32b625b5a60f063ea6
Niki Van Cleemput <niki@codemonkeys.be>
2013-05-30 18:22:24 +0200

    Added test 'doesitwork.js'

---
+++ test/test-doesitwork.js
@@ -0,0 +1,57 @@
+var assert = require('assert'),
+    events = require('events'),
+    eventcenter = require('../index.js');
+
+var e = new events.EventEmitter();
+
+var events                  = [], 
+    events_emitted          = [], 
+    events_handled          = [], 
+    center_events_handled   = [];
+
+var i;
+
+/*
+ * Creating a bunch of random events
+ */
+for(i = 0; i < 100; i++ ){
+    events.push( 'test_'+i );
+}
+
+events.forEach(function(eventName, i){
+    e.on(eventName, function(){
+        events_handled.push(eventName);
+    });
+});
+
+/*
+ * Watch the events with the eventcenter
+ */
+eventcenter.watch(e);
+
+eventcenter.on('event', function(eventName){
+        center_events_handled.push(eventName);
+});
+
+
+/*
+ * Emit the events one by one
+ */
+events.forEach(function(eventName, i){
+    e.emit(eventName);
+    events_emitted.push(eventName);
+});
+
+
+process.on('exit', function() {
+
+    assert.deepEqual(events, events_emitted, 
+                     'We should have emitted all events');
+    assert.deepEqual(events_emitted, events_handled, 
+                     'The standard handler should have handled all the events');
+    assert.deepEqual(events_handled, center_events_handled,
+                     'The EventCenter should have handled all the events');
+
+});
+
+
